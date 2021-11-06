87387d1e61d771d9fc10c8b357009240a82a324c
Denis Lesnik <denis.lesnik@jetbrains.com>
2021-11-06 14:23:14 +0100

    DP: Rucksack. Added top to bottom solution

--- src/Tasks/Dynamic Programming/Rucksack/Sack.test.ts
+++ src/Tasks/Dynamic Programming/Rucksack/Sack.test.ts
@@ -1,27 +1,16 @@
-import {solveKnapsackBrute} from "./Sack";
+import {solveKnapsackBrute, solveKnapsackTopToBottom} from "./Sack";
 
 describe("Knapsack issue", () => {
-  it("Input #1.", () => {
-    const profits = [1, 6, 10, 16];
-    const weights = [1, 2, 3, 5];
-    const capacity = 7;
-
-    expect(solveKnapsackBrute(profits, weights, capacity)).toBe(22);
+  it("Brute force works", () => {
+    expect(solveKnapsackBrute([1, 6, 10, 16], [1, 2, 3, 5], 7)).toBe(22);
+    expect(solveKnapsackBrute([1, 6, 10, 16], [1, 2, 3, 5], 6)).toBe(17);
+    expect(solveKnapsackBrute([8, 13, 10, 16], [1, 2, 3, 5], 6)).toBe(31);
   });
 
-  it("Input #2.", () => {
-    const profits = [1, 6, 10, 16];
-    const weights = [1, 2, 3, 5];
-    const capacity = 6;
-
-    expect(solveKnapsackBrute(profits, weights, capacity)).toBe(17);
-  });
-
-  it("Input #3.", () => {
-    const profits = [8, 13, 10, 16];
-    const weights = [1, 1, 2, 5];
-    const capacity = 6;
-
-    expect(solveKnapsackBrute(profits, weights, capacity)).toBe(31);
+  it("Top to bottom", () => {
+    expect(solveKnapsackTopToBottom([1, 6, 10, 16], [1, 2, 3, 5], 7)).toBe(22);
+    expect(solveKnapsackTopToBottom([1, 6, 10, 16], [1, 2, 3, 5], 6)).toBe(17);
+    expect(solveKnapsackTopToBottom([8, 13, 10, 16], [1, 2, 3, 5], 6)).toBe(31);
   });
 });
+
--- src/Tasks/Dynamic Programming/Rucksack/Sack.ts
+++ src/Tasks/Dynamic Programming/Rucksack/Sack.ts
@@ -12,10 +12,38 @@ export const solveKnapsackBrute = function(profits: Array<number>, weights: Arra
 
     const profitForRestItems = addItemTo(capacityLeft, index + 1);
 
-    console.log(capacityLeft, index, profitForRestItems, profitForCurrentItem)
     return Math.max(profitForCurrentItem, profitForRestItems)
   }
 
+
+  return addItemTo(capacity, 0);
+};
+
+export const solveKnapsackTopToBottom = function(profits: Array<number>, weights: Array<number>, capacity: number) {
+  const dp: number[][] = [];
+  const addItemTo = (capacityLeft: number, index: number): number => {
+    if (capacityLeft <= 0 || index >= profits.length) {
+      return 0;
+    }
+
+    if (dp[capacityLeft] == null) {
+      dp[capacityLeft] = [];
+    }
+
+    if (dp[capacityLeft][index] != null) {
+      return dp[capacityLeft][index];
+    }
+
+    let profitForCurrentItem = 0;
+
+    if (weights[index] <= capacityLeft) {
+      profitForCurrentItem = profits[index] + addItemTo(capacityLeft - weights[index], index + 1);
+    }
+
+    const profitForRestItems = addItemTo(capacityLeft, index + 1);
+    dp[capacityLeft][index] = Math.max(profitForCurrentItem, profitForRestItems)
+    return dp[capacityLeft][index]
+  }
 
   return addItemTo(capacity, 0);
 };
