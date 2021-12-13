8efbf27bae67860836bf822f88727df62079480b
Denis Lesnik <denis.lesnik@jetbrains.com>
2021-12-14 00:05:13 +0100

    278. First Bad version

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  globals: {
+    'ts-jest': {diagnostics: false}
+  },
   testEnvironment: 'node',
   transform: {
     "^.+\\.tsx?$": "ts-jest"
---
+++ src/BinarySearch/278. First Bad Version/FirstBadVersion.test.ts
@@ -0,0 +1,14 @@
+import {random} from "../../Utils/Random";
+import {firstBadVersion} from "./FirstBadVersion";
+
+describe("FirstBadVersion", () => {
+  it("Works", () => {
+    const min = 0;
+    const max = 100;
+    const badStart = random(min, max);
+    const arr = Array.from({length: max}, (_, index) => index < badStart);
+
+    expect(firstBadVersion([true, true, true, false])).toBe(3);
+    expect(firstBadVersion(arr)).toBe(badStart);
+  });
+});
